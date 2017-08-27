4337800987f1f1465c999856ae483a98d02156a0
bglee <deptno@gmail.com>
2017-08-27 20:03:31 +0900

    refactoring

--- index.d.ts
+++ index.d.ts
@@ -3,6 +3,7 @@ export interface FxHook<S> {
 }
 export interface Transform<S> {
     query?(query: any, getState: () => S): any;
+    body?(body: any, getState: () => S): any;
 }
 export interface BaseOption<S> {
     headers?: any;
@@ -11,29 +12,31 @@ export interface BaseOption<S> {
     fail?: FxHook<S>;
     condition?(...args: any[]): boolean;
 }
-export interface GetOption<S> extends BaseOption<S> {
-    query?: any;
-}
-export declare const GET: <S>(url: any, actions: [string, string, string], {query, headers, condition, success, fail, transform}?: GetOption<S>) => (dispatch: any, getState: any, extraArgs: any) => Promise<boolean>;
-export interface PostOption<S> extends BaseOption<S> {
-    body?: any;
-}
-export declare const POST: <S>(url: any, actions: [string, string, string], {body, headers, success, fail}?: PostOption<S>) => (dispatch: any, getState: any, extraArgs: any) => Promise<boolean>;
-export interface PutOption<S> extends BaseOption<S> {
-    body?: any;
-}
-export declare const PUT: <S>(url: any, actions: [string, string, string], {headers, body, success}?: PutOption<S>) => (dispatch: any, getState: any, extraArgs: any) => Promise<boolean>;
-export interface PatchOption<S> extends BaseOption<S> {
-    body?: any;
-}
-export declare const PATCH: <S>(url: any, actions: [string, string, string], {headers, body, success, fail}?: PatchOption<S>) => (dispatch: any, getState: any, extraArgs: any) => Promise<boolean>;
-export interface PatchOption<S> extends BaseOption<S> {
-    query?: any;
-}
-export declare const DELETE: <S>(url: any, actions: string[], {headers, query}?: PatchOption<S>) => (dispatch: any, getState: any) => Promise<boolean>;
+export declare type Actions = [string, string, string];
 export interface ReduxFetchAction {
     type: string;
     query?: any;
+    body?: any;
     error?: Error;
+    payload?: any;
+}
+export interface GetOption<S> extends BaseOption<S> {
+    query?: any;
+}
+export interface PostOption<S> extends BaseOption<S> {
     body?: any;
 }
+export interface PutOption<S> extends BaseOption<S> {
+    body?: any;
+}
+export interface PatchOption<S> extends BaseOption<S> {
+    body?: any;
+}
+export interface DeleteOption<S> extends BaseOption<S> {
+    query?: any;
+}
+export declare function GET<S>(url: any, actions: Actions, a?: GetOption<S>): (dispatch: any, getState: any, extraArgs: any) => Promise<boolean>;
+export declare function POST<S>(url: any, actions: Actions, a?: GetOption<S>): (dispatch: any, getState: any, extraArgs: any) => Promise<boolean>;
+export declare function PUT<S>(url: any, actions: Actions, a?: GetOption<S>): (dispatch: any, getState: any, extraArgs: any) => Promise<boolean>;
+export declare function PATCH<S>(url: any, actions: Actions, a?: GetOption<S>): (dispatch: any, getState: any, extraArgs: any) => Promise<boolean>;
+export declare function DELETE<S>(url: any, actions: Actions, a?: GetOption<S>): (dispatch: any, getState: any, extraArgs: any) => Promise<boolean>;
--- index.ts
+++ index.ts
@@ -6,6 +6,7 @@ export interface FxHook<S> {
 }
 export interface Transform<S> {
   query?(query: any, getState: () => S): any
+  body?(body: any, getState: () => S): any
 }
 export interface BaseOption<S> {
   headers?: any
@@ -15,30 +16,82 @@ export interface BaseOption<S> {
 
   condition?(...args): boolean
 }
+
+export type Actions = [string, string, string]
+export interface ReduxFetchAction {
+  type: string
+  query?: any
+  body?: any
+  error?: Error
+  payload?: any
+}
 export interface GetOption<S> extends BaseOption<S> {
   query?: any
 }
+export interface PostOption<S> extends BaseOption<S> {
+  body?: any
+}
+export interface PutOption<S> extends BaseOption<S> {
+  body?: any
+}
+export interface PatchOption<S> extends BaseOption<S> {
+  body?: any
+}
+export interface DeleteOption<S> extends BaseOption<S> {
+  query?: any
+}
 
-export const GET = <S>(url, actions: Actions, {query, headers, condition, success, fail, transform = {}}: GetOption<S> = {}) => {
-  const [pending, ok, err] = actions
+export function GET<S>(url, actions: Actions, a: GetOption<S> = {}) {
+  return common(url, actions, {...a, method: 'GET'} as any)
+}
+export function POST<S>(url, actions: Actions, a: GetOption<S> = {}) {
+  return common(url, actions, {...a, method: 'POST'} as any)
+}
+export function PUT<S>(url, actions: Actions, a: GetOption<S> = {}) {
+  return common(url, actions, {...a, method: 'PUT'} as any)
+}
+export function PATCH<S>(url, actions: Actions, a: GetOption<S> = {}) {
+  return common(url, actions, {...a, method: 'PATCH'} as any)
+}
+export function DELETE<S>(url, actions: Actions, a: GetOption<S> = {}) {
+  return common(url, actions, {...a, method: 'DELETE'} as any)
+}
+
+interface RequestParam {
+  query?: any
+  body?: any
+}
+
+function _transform<S>(getState: () => S, params: RequestParam, transformer: Transform<S>) {
+  const ret = {}
+  for (let key in transformer) {
+    ret[key] = transformer[key](params[key])
+  }
+  return ret as any
+}
+
+function common<S>(url, actions: Actions, a: BaseOption<S> & { method: string, query: any, body: any } = {} as any) {
+  const [pending, ok, err]                                                     = actions
+  let {method, query, body, headers, condition, success, fail, transform = {}} = a
+
   return async (dispatch, getState, extraArgs) => {
     if (condition && !condition(dispatch, getState, extraArgs)) {
       return false
     }
-    if (transform.query) {
-      query = transform.query(query, getState)
-    }
-    dispatch({
-      type: pending,
-      query
-    })
-    const target = query ? `${url}?${stringify(query)}` : url
 
+    const {query, body} = _transform(getState, {query: a.query, body: a.body}, transform)
+    //todo: dispatch reuqest
+    dispatch({type: pending, query})
     try {
-      const response = await fetch(target, {
-        method: 'GET',
-        headers
-      })
+      const target     = query ? `${url}?${stringify(query)}` : url
+      const param: any = {method, headers}
+
+      if (body) {
+        param.body = JSON.stringify(body)
+      }
+
+      const response = await fetch(target, param)
+
       if (response.status >= 400) {
         throw await handleError(response)
       }
@@ -48,193 +101,26 @@ export const GET = <S>(url, actions: Actions, {query, headers, condition, succes
         ? success(dispatch, getState, extraArgs, json)
         : json
 
-      dispatch({
-        type: ok,
-        query,
-        payload
-      })
+      //todo: dispatch success
+      dispatch({type: ok, query, body, payload})
       return true
     } catch (error) {
       console.error(err, error)
+      //todo: dispatch fail
       dispatch({
         type:  err,
         error: fail ? fail(dispatch, getState, extraArgs, error) : error,
         query,
+        body
       })
       return false
     }
   }
 }
-
-export interface PostOption<S> extends BaseOption<S> {
-  body?: any
-}
-
-export const POST = <S>(url, actions: Actions, {body, headers, success, fail}: PostOption<S> = {}) => {
-  const [pending, ok, err] = actions
-
-  return async (dispatch, getState, extraArgs) => {
-    dispatch({type: pending})
-    try {
-      const response = await fetch(url, {
-        method: 'POST',
-        headers,
-        body:   JSON.stringify(body)
-      })
-      if (response.status >= 400) {
-        throw await handleError(response)
-      }
-      const json    = await response.json()
-      const payload = success
-        ? success(dispatch, getState, extraArgs, json)
-        : json
-
-      dispatch({
-        type: ok,
-        body,
-        payload
-      })
-      return true
-    } catch (error) {
-      dispatch({
-        type:  err,
-        body,
-        error: fail ? fail(dispatch, getState, extraArgs, error) : error
-      })
-      return false
-    }
-  }
-}
-
-export interface PutOption<S> extends BaseOption<S> {
-  body?: any
-}
-
-export const PUT = <S>(url, actions: Actions, {headers, body, success}: PutOption<S> = {}) => {
-  const [pending, ok, err] = actions
-
-  return async (dispatch, getState, extraArgs) => {
-    dispatch({type: pending})
-    try {
-      const response = await fetch(url, {
-        method: 'PUT',
-        headers,
-        body:   JSON.stringify(body)
-      })
-      if (response.status >= 400) {
-        throw await handleError(response)
-      }
-      const json    = await response.json()
-      const payload = success
-        ? success(dispatch, getState, extraArgs, json)
-        : json
-
-      dispatch({
-        type: ok,
-        payload
-      })
-      return true
-    } catch (error) {
-      dispatch({
-        type: err,
-        error
-      })
-      return false
-    }
-  }
-}
-
-export interface PatchOption<S> extends BaseOption<S> {
-  body?: any
-}
-
-export const PATCH = <S>(url, actions: Actions, {headers, body, success, fail}: PatchOption<S> = {}) => {
-  const [pending, ok, err] = actions
-
-  return async (dispatch, getState, extraArgs) => {
-    dispatch({type: pending})
-    try {
-      const response = await fetch(url, {
-        headers,
-        method: 'PATCH',
-        body:   JSON.stringify(body)
-      })
-      if (response.status >= 400) {
-        throw await handleError(response)
-      }
-      const json    = await response.json()
-      const payload = success
-        ? success(dispatch, getState, extraArgs, json)
-        : json
-
-      dispatch({
-        type: ok,
-        payload
-      })
-      return true
-    } catch (error) {
-      dispatch({
-        type:  err,
-        error: fail ? fail(dispatch, getState, extraArgs, error) : error
-      })
-      return false
-    }
-  }
-}
-
-export interface PatchOption<S> extends BaseOption<S> {
-  query?: any
-}
-
-export const DELETE = <S>(url, actions: string[], {headers, query}: PatchOption<S> = {}) => {
-  const [pending, ok, err] = actions
-
-  return async (dispatch, getState) => {
-    dispatch({
-      type: pending,
-      query
-    })
-    const target = query ? `${url}?${stringify(query)}` : url
-
-    try {
-      const response = await fetch(target, {
-        headers,
-        method: 'DELETE',
-      })
-      if (response.status >= 400) {
-        throw await handleError(response)
-      }
-
-      dispatch({
-        type:    ok,
-        payload: response.json(),
-        query,
-      })
-      return true
-    } catch (error) {
-      dispatch({
-        type: err,
-        query,
-        error
-      })
-      return false
-    }
-  }
-}
-
-const handleError = async response => {
-  const message = await response.json()
+async function handleError(response) {
   return new Error(JSON.stringify({
-    code: response.status,
-    message
+    code:    response.status,
+    message: await response.json()
   }))
 }
 
-type Actions = [string, string, string]
-
-export interface ReduxFetchAction {
-  type: string
-  query?: any
-  error?: Error
-  body?: any
-}
